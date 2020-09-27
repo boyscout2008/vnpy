5d12f1aff8a1704bc227aa1db2c4f995a74b9b7b
boyscout2008 <tqdada@hotmail.com>
2020-09-27 22:57:01 +0800

    add bubugao strategy, TODO: panzhong_zd_long

--- ibstocks/bubugao.py
+++ ibstocks/bubugao.py
@@ -7,8 +7,6 @@ control three close modes:
 Another parameter "duobeili_threshold" is used to decide a valid long trend, which is equity dependent.
 NOTE: Variable zz_count will reset for new long trend
 TODO: limit order with given price
-trick: 单向滞涨止盈策略系统支持不好，采用另类方式实现，就是滞涨开空，实际上就跟手动开多的仓位相抵消了。
-具体改动涉及：init_pos，和pos的判断
 """
 
 from vnpy.app.cta_strategy import (
@@ -29,29 +27,32 @@ import winsound
 import bs_vn_base as bs
 
 #partial bubugao strategy with close fuction while zhizhang
-class BubugaoZZ_short(CtaTemplate):
+class BubugaoZZ(CtaTemplate):
     """"""
     author = "boyscout"
 
     zz_count_max = 1
     above_zz_1 = 0.0#1.001
     duobeili_threshold = 1.015 # 美股个股、取货品种都各不相同
-    init_long_pos = 0 #实盘用：手动开仓仓位设定，从而启动单向滞涨止盈策略
+    dst_long_pos = 0
     cover_before_close = True
     last_15mins_bar_index = 375
-    backtest_flag = False #回测用
+    zy_threshold = 1.04
+    #backtest_flag = False #回测用
 
     median_start = 1
     zz_count = 0
     zz_1_high = 0.0
     partial_pos_count = 0
+    actual_long_pos = 0
 
-    parameters = ["zz_count_max", "above_zz_1", "duobeili_threshold", "init_long_pos", "cover_before_close", "last_15mins_bar_index", "backtest_flag"]
-    variables = ["median_start", "zz_count", "zz_1_high", "partial_pos_count"]
+
+    parameters = ["zz_count_max", "above_zz_1", "duobeili_threshold", "dst_long_pos", "cover_before_close", "last_15mins_bar_index", "zy_threshold"]
+    variables = ["median_start", "zz_count", "zz_1_high", "partial_pos_count", "actual_long_pos"]
 
     def __init__(self, cta_engine, strategy_name, vt_symbol, setting):
         """"""
-        super(BubugaoZZ_short, self).__init__(
+        super(BubugaoZZ, self).__init__(
             cta_engine, strategy_name, vt_symbol, setting
         )
 
@@ -117,22 +118,15 @@ class BubugaoZZ_short(CtaTemplate):
 
         num_bar = len(mk)
 
-        # 回测设置：早盘固定时间开仓，滞涨止盈回测用
-        if self.backtest_flag and num_bar == 15:
-            self.buy(mk["close"][-1], 1, True)
-            self.write_log("simulated buy at price %.2f for backtesting zz"%(mk["close"][-1]))
-
         # 尾盘强制平仓
         if num_bar >= self.last_15mins_bar_index and self.cover_before_close:
-            if self.pos != -self.init_long_pos and self.init_long_pos > 0:
+            if self.pos > 0:
                 self.cancel_all() # 取消未成交订单，重新下单
-                cur_stop_pos = self.init_long_pos + self.pos
-                assert(cur_stop_pos > 0)
-                self.sell(mk["close"][-1], cur_stop_pos, True)
+                self.sell(mk["close"][-1], self.pos, True)
                 self.write_log("weipan pingcang %d at price %.2f"%(cur_stop_pos, mk["close"][-1]))
                 #winsound.PlaySound(bs.SOUND_NOTICE_ORDER, winsound.SND_FILENAME)
             return
-        
+
         mk = mk.assign(vwap = ((mk["volume"]*mk["close"]).cumsum() / mk["volume"].cumsum()).ffill())
         # 重置参数，重新开始波段计算
         if num_bar == 1 or mk['close'][-1] <= mk['vwap'][-1]:
@@ -140,24 +134,33 @@ class BubugaoZZ_short(CtaTemplate):
             self.zz_count = 0
             self.zz_1_high = 0.0
 
-        if num_bar > 30:
-            # 市价单3分钟只是部分成交则提醒人工干预
-            if self.pos < 0 and self.pos != -self.init_long_pos:
+        # 步步高策略1： 21:45偏多走稳追涨
+        #TODO：步步高策略2：相对低位止跌做多
+        if num_bar >= 15 and num_bar < 30:
+            if self.pos < self.dst_long_pos and (mk["price"][5:] >= mk["vwap"][5:]).all() and mk["price"].max() < mk["price"].min()*self.zy_threshold:
+                self.cancel_all()
+                self.buy(mk["close"][-1], self.dst_long_pos - self.pos, True)
+                self.write_log("kaicang_zz_2145 at price %.2f"%(mk["close"][-1]))
+        elif num_bar == 30:
+            self.cancel_all()
+            self.actual_long_pos = self.pos
+        elif num_bar > 30:
+            # 市价单平仓3分钟只是部分成交则提醒人工干预
+            if self.pos > 0 and self.pos != self.actual_long_pos:
                 self.partial_pos_count += 1
                 if self.partial_pos_count > 3:
                     self.write_log("市价单3分钟未成交，提醒人工介入!")
                     winsound.PlaySound(bs.SOUND_MANUAL_INTERUPT, winsound.SND_FILENAME)
             else:
                 self.partial_pos_count = 0
-                
+
             # case3：首次滞涨后突破止盈（微突破不止盈，可以考虑协同第二波多滞涨止盈一起工作）
             if self.above_zz_1 > 0.00001 and self.zz_count == 1 and mk["close"][-1] > self.zz_1_high*(1.0+self.above_zz_1):
                 self.write_log("ZZ %d times from %d, close long at %d with price: %.2f above zz_1_high: %.2f" \
                     %(self.zz_count, self.median_start, num_bar, mk["close"][-1], self.zz_1_high))
-                if self.pos == 0 and self.init_long_pos > 0:
+                if self.pos > 0:
                     self.cancel_all()
-                    self.sell(mk["close"][-1], abs(self.init_long_pos), True)
-                    #winsound.PlaySound(bs.SOUND_NOTICE_ORDER, winsound.SND_FILENAME)
+                    self.sell(mk["close"][-1], abs(self.pos), True)
                 self.zz_count= self.zz_count_max + 1 #disable zz modes
 
             if num_bar - self.median_start > 30:
@@ -171,21 +174,17 @@ class BubugaoZZ_short(CtaTemplate):
                         #case1&2: close long while zz 1 or 2 times
                         if self.zz_count >= self.zz_count_max and (self.above_zz_1 > -0.00001 and self.above_zz_1 < 0.00001):
                             self.write_log("ZZ %d times from %d, suggest PC at %d with price %.2f"%(self.zz_count, self.median_start, num_bar, mk["close"][-1]))
-                            if self.pos == 0 and self.init_long_pos > 0 \
-                                and mk["close"][-1] > (mk["vwap"][-1] + (median_CH - mk["vwap"][-1])*0.5):
+                            if self.pos > 0 and mk["close"][-1] > (mk["vwap"][-1] + (median_CH - mk["vwap"][-1])*0.5):
                                 self.cancel_all()
-                                self.sell(mk["close"][-1], abs(self.init_long_pos), True)
-                                #winsound.PlaySound(bs.SOUND_NOTICE_ORDER, winsound.SND_FILENAME)
+                                self.sell(mk["close"][-1], abs(self.pos), True)
                     elif len(median_h_zz) > 30:
                         #The backup logic for unexpected network breakup while len(median_h_zz) == 30, which lasts for 30 minutes,
                         #Meanwhile it's used for sell with limit price.
                         #case1&2: close long while zz 1 or 2 times
                         if self.zz_count >= self.zz_count_max and (self.above_zz_1 > -0.00001 and self.above_zz_1 < 0.00001):
-                            if self.pos == 0 and self.init_long_pos > 0 \
-                                and mk["close"][-1] > (mk["vwap"][-1] + (median_CH - mk["vwap"][-1])*0.5):
+                            if self.pos > 0 and mk["close"][-1] > (mk["vwap"][-1] + (median_CH - mk["vwap"][-1])*0.5):
                                 self.cancel_all()
-                                self.sell(mk["close"][-1], abs(self.init_long_pos), True)
-                                #winsound.PlaySound(bs.SOUND_NOTICE_ORDER, winsound.SND_FILENAME)
+                                self.sell(mk["close"][-1], abs(self.pos), True)
         
     def on_order(self, order: OrderData):
         """
